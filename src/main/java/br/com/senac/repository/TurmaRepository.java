22f2fdac195ce543086b3c79a6c31359e5f5207c
brunodias-r <bruno.brd73@gmail.com>
2022-11-17 15:47:24 -0300

    Alteração nos repository.

--- src/main/java/br/com/senac/repository/TurmaRepository.java
+++ src/main/java/br/com/senac/repository/TurmaRepository.java
@@ -1,3 +1,27 @@
+//package br.com.senac.repository;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.stereotype.Repository;
+//import br.com.senac.entity.Turma;
+//
+//@Repository
+//public interface TurmaRepository extends JpaRepository<Turma, Integer>{
+//	
+//	@Query("SELECT DISTINCT t FROM Turma t"
+//			+ " JOIN FETCH t.alunos a"
+//			+ " WHERE a.turma.id = ?1")
+//	List<Turma> findAllByIdTurma(Integer idTurma);
+//	
+//	@Query("SELECT t FROM Turma t"
+//			+ " LEFT JOIN t.aluno a"
+//			+ " WHERE a.turma.id = ?1")
+//	Turma findTurmaByIdTurma(Integer idturma);
+//	
+//}
+
 package br.com.senac.repository;
 
 import java.util.List;
@@ -5,14 +29,16 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import br.com.senac.entity.Turma;
 
 @Repository
-public interface TurmaRepository extends JpaRepository<Turma, Integer>{
-	
-	@Query("SELECT DISTINCT t FROM Turma t"
-			+ " JOIN FETCH t.alunos a"
-			+ " WHERE a.turma.id = ?1")
+public interface TurmaRepository extends JpaRepository<Turma, Integer> {
+
+	@Query("SELECT DISTINCT t FROM Turma t" + " JOIN FETCH t.alunos a" + " WHERE a.turma.id = ?1")
 	List<Turma> findAllByIdTurma(Integer idTurma);
+
+	@Query("SELECT t FROM Turma t" + " LEFT JOIN t.alunos a" + " WHERE a.turma.id = ?1")
+	Turma findTurmaByIdTurma(Integer id);
 	
 }
